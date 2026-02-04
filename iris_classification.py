c9d9ca9adf279f6d96e0eef2d7a842dec73909b4
shamal9272 <sasteshamal31@gmail.com>
2026-02-04 01:19:46 -0800

    Add the specific change

--- iris_classification.py
+++ iris_classification.py
@@ -58,38 +58,3 @@ plt.title("Confusion Matrix - Iris Flower Classification")
 plt.savefig("output.png")
 plt.show()
 
-=======
-# Iris Flower Classification using Machine Learning
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
-
-# Load dataset
-data = pd.read_csv("Iris.csv")
-
-# Drop Id column
-data.drop("Id", axis=1, inplace=True)
-
-# Features and target
-X = data.drop("Species", axis=1)
-y = data["Species"]
-
-# Split dataset
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-# Train model
-model = LogisticRegression(max_iter=200)
-model.fit(X_train, y_train)
-
-# Predictions
-y_pred = model.predict(X_test)
-
-# Results
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-print("\nClassification Report:\n")
-print(classification_report(y_test, y_pred))
